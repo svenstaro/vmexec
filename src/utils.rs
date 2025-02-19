28ce00aa562a78f1ec9dfb9f98f8840f3275f463
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-02-19 04:23:24 +0100

    The forgotten file

---
+++ src/utils.rs
@@ -0,0 +1,33 @@
+/// Path escaping, like `systemd-escape --path`.
+///
+/// From https://github.com/lucab/libsystemd-rs/blob/b43fa5e3b5eca3e6aa16a6c2fad87220dc0ad7a0/src/unit.rs
+pub fn escape_path(path: &str) -> String {
+    let trimmed = path.trim_matches('/');
+    if trimmed.is_empty() {
+        return "-".to_string();
+    }
+
+    let mut slash_seq = false;
+    let parts: Vec<String> = trimmed
+        .bytes()
+        .filter(|b| {
+            let is_slash = *b == b'/';
+            let res = !(is_slash && slash_seq);
+            slash_seq = is_slash;
+            res
+        })
+        .enumerate()
+        .map(|(n, b)| escape_byte(b, n))
+        .collect();
+    parts.join("")
+}
+
+fn escape_byte(b: u8, index: usize) -> String {
+    let c = char::from(b);
+    match c {
+        '/' => '-'.to_string(),
+        ':' | '_' | '0'..='9' | 'a'..='z' | 'A'..='Z' => c.to_string(),
+        '.' if index > 0 => c.to_string(),
+        _ => format!(r#"\x{:02x}"#, b),
+    }
+}
