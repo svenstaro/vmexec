0f7056b8536c7831752c61723ca3b2d5cd32be91
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-02-23 18:41:42 +0100

    Restructure download logic a bit to be neater

--- src/vm_images.rs
+++ src/vm_images.rs
@@ -12,112 +12,13 @@ use url::Url;
 
 use crate::cli::Pull;
 
-/// Retrieve the latest locally available Arch Linux image
-pub async fn get_latest_local_archlinux_image(distro_image_dir: &Path) -> Result<Option<PathBuf>> {
-    let mut images = vec![];
-
-    // First we'll gather a list of all local images.
-    let mut entries = fs::read_dir(distro_image_dir).await?;
-    while let Some(entry) = entries.next_entry().await? {
-        if let Some(ext) = entry.path().extension() {
-            let filename = entry
-                .file_name()
-                .into_string()
-                .map_err(|e| eyre!("Filename invalid somehow: {e:?}"))?;
-            if entry.file_type().await?.is_file()
-                && filename.starts_with("Arch-Linux")
-                && ext == "qcow2"
-            {
-                images.push(entry.path());
-            }
-        }
-    }
-
-    // Sort the images by name as the Arch Linux images contain the ISO date so that the latest one
-    // will be the one at the very end.
-    images.sort();
-
-    let latest_image = images.last().cloned();
-    Ok(latest_image)
-}
-
-/// Download the most recent Arch Linux image
-#[instrument]
-pub async fn ensure_archlinux_image(cache_dir: &Path, pull: Pull) -> Result<PathBuf> {
-    let distro_image_dir = cache_dir.join("images").join("archlinux");
-    if !distro_image_dir.exists() {
-        debug!("Distro image dir {distro_image_dir:?} doesn't exist yet, creating");
-        std::fs::create_dir_all(&distro_image_dir)
-            .wrap_err(format!("Creating distro image dir {distro_image_dir:?}"))?;
-    }
-
-    let latest_local_image = get_latest_local_archlinux_image(&distro_image_dir).await?;
-
-    // If --pull missing was provided and if there is already a local image (no matter the age),
-    // we'll just return that. If there's no image we'll do nothing which will cause it to be
-    // downloaded later in the function.
-    if pull == Pull::Missing {
-        if let Some(latest) = latest_local_image {
-            return Ok(latest);
-        }
-    }
-
-    // If --pull missing was provided and if there is already a local image (no matter the age),
-    // we'll just return that. If there's no image we'll error out.
-    if pull == Pull::Never {
-        if let Some(latest) = latest_local_image {
-            return Ok(latest);
-        } else {
-            bail!("No local image found and `--pull never` selected, bailing");
-        }
-    }
-
-    debug!("Latest local image is {latest_local_image:?}");
-
-    let arch_boxes_base_url = Url::parse(
-        "https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/artifacts/master/raw/",
-    )?;
-    let build_version_url = arch_boxes_base_url.join("build.env?job=build:secure")?;
-
-    // Figure out what the current version is.
-    let build_version = reqwest::get(build_version_url)
-        .await?
-        .error_for_status()?
-        .text()
-        .await?;
-    let build_version_line = build_version
-        .lines()
-        .next()
-        .ok_or_eyre("No line break in output")?;
-    let build_version = build_version_line
-        .split('=')
-        .next_back()
-        .ok_or_eyre(format!(
-            "BUILD_VERSION line not in expected format: {build_version_line}"
-        ))?;
-
-    let image_name = format!("Arch-Linux-x86_64-libvirt-executor-{build_version}.qcow2");
-
-    debug!("Latest remote image is: {image_name}");
-
-    // The file will be downloaded to this path.
-    let local_image_path = distro_image_dir.join(&image_name);
-
-    let image_ext = local_image_path
-        .extension()
-        .ok_or_eyre("Somehow the image '{local_image_path:?}' didn't have a file extension")?
-        .to_str()
-        .ok_or_eyre("File extension in '{image_ext}' isn't ASCII")?;
-    let local_image_checksum_path = local_image_path.with_extension(format!("{image_ext}.SHA256"));
-
-    // First check if the files even need downloading or whether we already have them.
-    if local_image_path.exists() && local_image_checksum_path.exists() {
-        debug!("Found pre-existing files for image, skipping download");
-        return Ok(local_image_path);
-    } else if pull == Pull::Newer {
-        debug!("Didn't find requested image locally, proceeding to download");
-    }
-
+/// Download Arch Linux image
+pub async fn download_archlinux_image(
+    local_image_path: &Path,
+    local_image_checksum_path: &Path,
+    arch_boxes_base_url: &Url,
+    image_name: &str,
+) -> Result<()> {
     let image_url = arch_boxes_base_url.join(&format!("output/{image_name}?job=build:secure"))?;
     let image_checksum_url =
         arch_boxes_base_url.join(&format!("output/{image_name}.SHA256?job=build:secure"))?;
@@ -178,6 +79,129 @@ pub async fn ensure_archlinux_image(cache_dir: &Path, pull: Pull) -> Result<Path
     if image_checksum_raw != computed_checksum.as_slice() {
         bail!("Checksum mismatch on {local_image_path:?}, maybe the file got corrupted somehow. Try deleting it and retrying.");
     }
+
+    Ok(())
+}
+
+/// Retrieve the latest locally available Arch Linux image
+pub async fn get_latest_local_archlinux_image(distro_image_dir: &Path) -> Result<Option<PathBuf>> {
+    let mut images = vec![];
+
+    // First we'll gather a list of all local images.
+    let mut entries = fs::read_dir(distro_image_dir).await?;
+    while let Some(entry) = entries.next_entry().await? {
+        if let Some(ext) = entry.path().extension() {
+            let filename = entry
+                .file_name()
+                .into_string()
+                .map_err(|e| eyre!("Filename invalid somehow: {e:?}"))?;
+            if entry.file_type().await?.is_file()
+                && filename.starts_with("Arch-Linux")
+                && ext == "qcow2"
+            {
+                images.push(entry.path());
+            }
+        }
+    }
+
+    // Sort the images by name as the Arch Linux images contain the ISO date so that the latest one
+    // will be the one at the very end.
+    images.sort();
+
+    let latest_image = images.last().cloned();
+    Ok(latest_image)
+}
+
+/// Download the most recent Arch Linux image
+#[instrument]
+pub async fn ensure_archlinux_image(cache_dir: &Path, pull: Pull) -> Result<PathBuf> {
+    let distro_image_dir = cache_dir.join("images").join("archlinux");
+    if !distro_image_dir.exists() {
+        debug!("Distro image dir {distro_image_dir:?} doesn't exist yet, creating");
+        std::fs::create_dir_all(&distro_image_dir)
+            .wrap_err(format!("Creating distro image dir {distro_image_dir:?}"))?;
+    }
+
+    let latest_local_image = get_latest_local_archlinux_image(&distro_image_dir).await?;
+
+    match pull {
+        // If --pull missing was provided and if there is already a local image (no matter the age),
+        // we'll just return that. If there's no image we'll do nothing which will cause it to be
+        // downloaded later in the function.
+        Pull::Missing => {
+            if let Some(latest) = latest_local_image {
+                info!("Found local image {latest:?} and \"--pull missing\" was provided so this is the image we're using");
+                return Ok(latest);
+            }
+        }
+        // If --pull missing was provided and if there is already a local image (no matter the age),
+        // we'll just return that. If there's no image we'll error out.
+        Pull::Never => {
+            if let Some(latest) = latest_local_image {
+                info!("Found local image {latest:?} and \"--pull never\" was provided so this is the image we're using");
+                return Ok(latest);
+            } else {
+                bail!("No local image found and `--pull never` selected, bailing");
+            }
+        }
+        Pull::Newer => {
+            if let Some(latest) = latest_local_image {
+                info!("Found local image {latest:?} but there might be a newer image so we're checking");
+            }
+        }
+    }
+
+    let arch_boxes_base_url = Url::parse(
+        "https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/artifacts/master/raw/",
+    )?;
+    let build_version_url = arch_boxes_base_url.join("build.env?job=build:secure")?;
+
+    // Figure out what the current version is.
+    let build_version = reqwest::get(build_version_url)
+        .await?
+        .error_for_status()?
+        .text()
+        .await?;
+    let build_version_line = build_version
+        .lines()
+        .next()
+        .ok_or_eyre("No line break in output")?;
+    let build_version = build_version_line
+        .split('=')
+        .next_back()
+        .ok_or_eyre(format!(
+            "BUILD_VERSION line not in expected format: {build_version_line}"
+        ))?;
+
+    let image_name = format!("Arch-Linux-x86_64-libvirt-executor-{build_version}.qcow2");
+
+    debug!("Latest remote image is: {image_name}");
+
+    // The file will be downloaded to this path.
+    let local_image_path = distro_image_dir.join(&image_name);
+
+    let image_ext = local_image_path
+        .extension()
+        .ok_or_eyre("Somehow the image '{local_image_path:?}' didn't have a file extension")?
+        .to_str()
+        .ok_or_eyre("File extension in '{image_ext}' isn't ASCII")?;
+    let local_image_checksum_path = local_image_path.with_extension(format!("{image_ext}.SHA256"));
+
+    // First check if the files even need downloading or whether we already have them.
+    if local_image_path.exists() && local_image_checksum_path.exists() {
+        debug!("Found pre-existing files for image, skipping download");
+        return Ok(local_image_path);
+    } else if pull == Pull::Newer {
+        debug!("Didn't find requested image locally, proceeding to download");
+    }
+
+    download_archlinux_image(
+        &local_image_path,
+        &local_image_checksum_path,
+        &arch_boxes_base_url,
+        &image_name,
+    )
+    .await?;
 
     Ok(local_image_path)
 }
